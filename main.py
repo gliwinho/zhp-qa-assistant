dd9d4cc0379016e1438510b9bf24d5efb42ad74b
jasiugliwa <166063478+jasiugliwa@users.noreply.github.com>
2024-04-05 21:11:16 +0200

    Revert "Feature/gpt 3.5 chain (#2)" (#3)
    This reverts commit e2559999e907fc15251bdc4cefd8185817144b62.

--- main.py
+++
@@ -1,9 +0,0 @@
-from core.chain import question_chain
-
-if __name__ == "__main__":
-    while True:
-        content = input('>> ')
-        output = question_chain.invoke({
-            "input": content
-        })
-        print(output["text"])
